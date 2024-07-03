790bd66fb9390bd3518f0d52ba4fa68aff7099ee
rahmteen <ramtin.khoee@gmail.com>
2024-07-03 13:09:41 -0700

    added docs + page routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 
+// pages
+import Home from "@/pages/Home/Home";
+import Admin from "@/pages/Admin/Admin";
+import NotFound from "@/pages/NotFound/NotFound";
+import Dashboard from "@/pages/Dashboard/Dashboard";
+import Register from "./pages/Register/Register";
+
+// wrappers
+// ...todo
+
+/**
+ * @name App
+ * @type {React.FC}
+ * @description - a component to manage global wrappers, app routes and page rendering.
+ * @return {React.ReactNode}
+ */
+
 const App: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<>Hello World!</>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,16 @@
 import ReactDOM from "react-dom/client";
-
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+
+import "@fontsource-variable/bricolage-grotesque";
+
 import { defaultTheme } from "@/theme";
 import App from "@/App";
 
-import "@fontsource-variable/bricolage-grotesque";
+/**
+ * @name root
+ * @description handles static imports, global providers and init state mgmt (todo). 
+ */
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
