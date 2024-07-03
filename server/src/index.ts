58691b325788c31452c214d89bb0ccebe4fb92a3
rahmteen <ramtin.khoee@gmail.com>
2024-07-03 12:43:18 -0700

    added cors/types and fixed bug + added client url to env

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 
 app.use(express.json());
 
